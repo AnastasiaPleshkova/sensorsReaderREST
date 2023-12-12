f2421cc3cbabad4c9e42e56f30a1084227b2be88
Anastasia <anastasiiapleshkova@gmail.com>
2023-12-12 17:32:16 +0300

    first commit

---
+++ src/main/java/ru/pleshkova/sensorsReaderREST/SensorsReaderRestApplication.java
@@ -0,0 +1,13 @@
+package ru.pleshkova.sensorsReaderREST;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensorsReaderRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SensorsReaderRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/pleshkova/sensorsReaderREST/SensorsReaderRestApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.pleshkova.sensorsReaderREST;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensorsReaderRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
